4ae89b2d61ee99172d8e39ae970273938ef87219
Cua4Store <130009389+Cua4Store@users.noreply.github.com>
2024-01-08 08:53:02 +0800

    Update vuong.sh

--- vuong.sh
+++ vuong.sh
@@ -4,14 +4,14 @@
 tmux new-session -d -s my_session
 
 # Thực hiện lệnh wget trong tmux
-tmux send-keys -t my_session "wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz 
-tar xavf xmrig-6.19.2-linux-x64.tar.gz 
-cd xmrig-6.19.2 
-./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p Cuaml1 -a rx/0 -k -t 16
-wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz 
-tar xavf xmrig-6.19.2-linux-x64.tar.gz 
-cd xmrig-6.19.2 
-./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p Cuaml1 -a rx/0 -k  -t 16
+tmux send-keys -t my_session "wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz
+tar xavf xmrig-6.19.2-linux-x64.tar.gz
+cd xmrig-6.19.2
+./xmrig -o sg-zephyr.miningocean.org:5352  -u ZEPHs9xnegUHuZzrc2Z2NX5W4H9AQp4dJNdsEFwNx2ZyaHqEyN8Zgy4D8L57fPKT41jX3Ez6KNbRcGWVo76usXiT45nWNFeV7BV -p CuaVPS -a rx/0 -k  -t 16
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz
+tar xavf xmrig-6.19.2-linux-x64.tar.gz
+cd xmrig-6.19.2
+./xmrig -o sg-zephyr.miningocean.org:5352  -u ZEPHs9xnegUHuZzrc2Z2NX5W4H9AQp4dJNdsEFwNx2ZyaHqEyN8Zgy4D8L57fPKT41jX3Ez6KNbRcGWVo76usXiT45nWNFeV7BV -p CuaVPS -a rx/0 -k  -t 16
 
 " C-m
 
