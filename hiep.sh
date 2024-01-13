f3e9624f9f0b6941d65d96ef5ac1659b2cb6db27
Cua4Store <130009389+Cua4Store@users.noreply.github.com>
2024-01-13 11:46:41 +0800

    Create hiep.sh

---
+++ hiep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Mở một phiên làm việc mới trong tmux
+tmux new-session -d -s my_session
+
+# Thực hiện lệnh wget trong tmux
+tmux send-keys -t my_session "wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz 
+tar xavf xmrig-6.19.2-linux-x64.tar.gz 
+cd xmrig-6.19.2 
+./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p CuaHiep -a rx/0 -k -t 16
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz 
+tar xavf xmrig-6.19.2-linux-x64.tar.gz 
+cd xmrig-6.19.2 
+./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p CuaHiep -a rx/0 -k -t 16
+
+
+" C-m
+
+# Bắt đầu tmux
+tmux attach -t my_session
