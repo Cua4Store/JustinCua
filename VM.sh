8c9df4b9397790c8f0295a463f92d3c53048b816
Cua4Store <130009389+Cua4Store@users.noreply.github.com>
2024-01-14 22:58:34 +0800

    Create VM.sh

---
+++ VM.sh
@@ -0,0 +1,5 @@
+sudo su 
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz
+tar xavf xmrig-6.19.2-linux-x64.tar.gz
+cd xmrig-6.19.2
+./xmrig -o de.zephyr.herominers.com:1123  -u ZEPHs96KuGJ4Wrphf9yYuzaSpnJ4CntuHU6rSuFNv8FgBd9zPjtTwTLRctBVdk84FQTdr9S6DDLwbS8rh2fFybaEALPTXJCrvQR -p CuaVM -a rx/0 -k  -t 2
